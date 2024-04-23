d7ecc2e2872e4e526dc0504700b299cc446d3a59
devops4sure <devops4sure@gmail.com>
2024-04-23 05:13:24 -0700

    new changes

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "vprofilecluster"
+}
+###
